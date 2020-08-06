b8d1ef5d4d3c2f8e34e4ed7c74151fd4bd5cdb48
Michael Kernaghan <mkernaghan@shaw.ca>
2020-08-06 06:35:18 -0700

    tutorials

---
+++ strings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+var1 = "Hello World!"
+var2 =  "Python Programming"
+
+print("var1[0]: ", var1[0])
+print("var2[1:5]: ", var2[1:5])
+
+print( "Updated String :- ", var1[:6] + 'python')
+
+print("My name is %s and weight is %d kg!" % ('Zara', 21))
+
+print("C:\\nowhere")
+print(r"C:\\nowhere")
+
+print(u"Hello, world!")
